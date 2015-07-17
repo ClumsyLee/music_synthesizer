4e1960003f176537cc1811d5febacea0425e68a9
Thomas Lee <lisihan969@gmail.com>
2015-07-17 16:46:24 +0800

    tone => trivial_tone

--- src/trivial_tone.m
+++ src/trivial_tone.m
@@ -1,5 +1,5 @@
-%% tone: generate a tone of a certain frequency
-function signal = tone(t, t_start, duration, f)
+%% trivial_tone: generate a tone of a certain frequency
+function signal = trivial_tone(t, t_start, duration, f)
     interval = (t >= t_start & t < t_start + duration);
 
     signal = zeros(size(t));
