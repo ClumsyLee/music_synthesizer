2d2da97238c232fd25144e8f6684d1813dafa428
Thomas Lee <lisihan969@gmail.com>
2015-07-18 14:15:51 +0800

    Clean code in harmonic_tone.m

--- src/harmonic_tone.m
+++ src/harmonic_tone.m
@@ -2,5 +2,5 @@
 function signal = harmonic_tone(t, t_start, duration, f)
     interval = (t >= t_start);
 
-    signal =  [1, 0.2, 0.3] * sin(2 * pi * f * [1:3]' * t);
-    signal = tone_shape(t - t_start, duration) .* signal;
+    shape = [1, 0.2, 0.3] * sin(2 * pi * f * [1:3]' * t);
+    signal = tone_shape(t - t_start, duration) .* shape;
