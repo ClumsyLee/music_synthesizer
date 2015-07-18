ec11389734b34ed37282be235b267752630e7c0d
Thomas Lee <lisihan969@gmail.com>
2015-07-18 13:07:47 +0800

    Reduce pause time

--- src/trivial_change_octave.m
+++ src/trivial_change_octave.m
@@ -1,6 +1,6 @@
 f_sample = 8e3;
 [t, music] = make_music(@refined_tone, f_sample);
 sound(music, 2 * f_sample);  % One octave higher.
-pause(5);
+pause(2.5);
 sound(music, 0.5 * f_sample);  % One octave lower.
 
