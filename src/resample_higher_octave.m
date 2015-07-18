a5b677ed5c47cc380645ec9bfa313593c273f9e2
Thomas Lee <lisihan969@gmail.com>
2015-07-18 10:55:08 +0800

    Calculate rational approximation in place

--- src/resample_higher_octave.m
+++ src/resample_higher_octave.m
@@ -1,3 +1,4 @@
 f_sample = 8e3;
+[Q, P] = rat(2^(1/12), 0.0000001);  % Find the rational approximation.
 [t, music] = make_music(@refined_tone, f_sample);
-sound(resample(music, 70, 99), f_sample);  % sqr(2) ~= 99 / 70
+sound(resample(music, P, Q), f_sample);
